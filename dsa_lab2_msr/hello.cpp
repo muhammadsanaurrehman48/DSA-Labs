21347d14dcacad5ac5ce0be74d4e111f5651d2f7
M Sana Ur Rehman <muhasana98@gmail.com>
2025-09-15 10:47:25 +0500

    Changes and then committed

--- dsa_lab2_msr/hello.cpp
+++ dsa_lab2_msr/hello.cpp
@@ -3,6 +3,10 @@ using namespace std;
 
 int main(){
     cout << "DSA LAB for Git Setting up" << endl;
+
+    //This is done so that we can get ourselves some changes done in the existing code file and push it to the repository with the commit
+    cout << "This is done so that we can get ourselves some changes done in the existing code file and push it to the repository with the commit" << endl;
+    
     return 0;
 }
 
---
+++ dsa_lab2_msr/second.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "This is the second file" << endl;
+    return 0;
+}
